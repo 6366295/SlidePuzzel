5d3bbd209a506b06ff289d28cdb15705f3391d1f
unknown <Ren@Ren-PC.(none)>
2012-06-22 15:51:34 +0200

    canvas game

--- src/com/multimedia/slidepuzzel/SlidePuzzelActivity.java
+++ src/com/multimedia/slidepuzzel/SlidePuzzelActivity.java
@@ -1,13 +1,24 @@
 package com.multimedia.slidepuzzel;
 
+import com.multimedia.slidepuzzel.R;
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
 
 public class SlidePuzzelActivity extends Activity {
+
+    private CameraView mCameraView;
+    
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.GameActivity);
+        
+        // get handles to the CameraView from XML
+        mCameraView = (CameraView) findViewById(R.id.cameraView1);
+        
+        mCameraView.setActivity(this);
     }
 }
