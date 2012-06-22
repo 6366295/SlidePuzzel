7d7ac4448a96c6054bb3fe77f196f172e0b7e014
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-06-22 20:05:25 +0200

    Camera werkend

--- src/com/multimedia/slidepuzzel/gameActivity.java
+++ src/com/multimedia/slidepuzzel/gameActivity.java
@@ -1,22 +1,14 @@
 package com.multimedia.slidepuzzel;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
-public class gameActivity extends Activity {
-    private CameraView mCameraView;
-
+public class GameActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
         
-        // get handles to the CameraView from XML
-        mCameraView = (CameraView) findViewById(R.id.cameraView1);
-        
-        mCameraView.setActivity(this);
-        
-        Intent intent = getIntent();
+        ((CameraView) findViewById(R.id.cameraView1)).setActivity(this);
     }
 }
