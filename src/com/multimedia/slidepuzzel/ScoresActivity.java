36ead6abe36a9804b6e0be39f84167ce3e1d3647
Mike Trieu <mt_tr1pl@hotmail.com>
2012-06-22 21:44:43 +0200

    tabs?

--- src/com/multimedia/slidepuzzel/ScoresActivity.java
+++ src/com/multimedia/slidepuzzel/ScoresActivity.java
@@ -1,14 +1,22 @@
 package com.multimedia.slidepuzzel;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScoresActivity extends Activity {
 	   @Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        
-	        Intent intent = getIntent();
+	        setContentView(R.layout.scores);
+	        
+	        Button back = (Button) findViewById(R.id.back2);
+	        back.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View view) {
+	                finish();
+	            }
+	        });
 	   }
 }
