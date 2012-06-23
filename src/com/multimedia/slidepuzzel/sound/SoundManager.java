4193ae8de0050d6bc7ec434927292334e43cca49
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-06-23 20:50:02 +0200

    Swap sound

--- src/com/multimedia/slidepuzzel/sound/SoundManager.java
+++ src/com/multimedia/slidepuzzel/sound/SoundManager.java
@@ -5,8 +5,9 @@ import com.multimedia.slidepuzzel.R;
 import android.content.Context;
 import android.media.SoundPool;
 import android.media.AudioManager;
+import android.util.Log;
 
-public class SoundManager{
+public class SoundManager implements SoundPool.OnLoadCompleteListener{
 	public int swap;
 	public int rotate;
 	public int win;
@@ -16,12 +17,19 @@ public class SoundManager{
 	// Build using getBaseContext()
 	public SoundManager(Context c){
 		pool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+		pool.setOnLoadCompleteListener(this);
 		swap = pool.load(c, R.raw.swap, 1);
 		rotate = pool.load(c, R.raw.rotate, 2);
 		win = pool.load(c, R.raw.win, 3);
 	}
 	
 	public int playSound(int soundId){
-		return pool.play(soundId, 1f, 1f, 1, 0, 1f);
+		return pool.play(soundId, 0.5f, 0.5f, 1, 0, 1f);
 	}
+	
+	public void onLoadComplete(SoundPool pool, int soundId, int status){
+		Log.d("SoundPool", "Sound " + soundId + " status " + status);
+	}
+	
+	
 }
