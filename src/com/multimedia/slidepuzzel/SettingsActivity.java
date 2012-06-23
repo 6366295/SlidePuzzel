85b14d18be7d95c8f179d752e1bbe0f6461d99c1
unknown <Ren@Ren-PC.(none)>
2012-06-23 14:19:16 +0200

    settingsmenu made but not working

--- src/com/multimedia/slidepuzzel/SettingsActivity.java
+++ src/com/multimedia/slidepuzzel/SettingsActivity.java
@@ -3,12 +3,22 @@ package com.multimedia.slidepuzzel;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends Activity {
 	   @Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        
-	        Intent intent = getIntent();
+	        setContentView(R.layout.settings);
+	        
+	        Button back = (Button) findViewById(R.id.back3);
+	        back.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View view) {
+	                finish();
+	            }
+	        });
 	   }
 }
+
