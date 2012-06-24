dbc95ab3e8011cc82a7490bb1bd11c9ea5f19e03
unknown <Ren@Ren-PC.(none)>
2012-06-24 16:22:10 +0200

    warning weg uit settings

--- src/com/multimedia/slidepuzzel/SettingsActivity.java
+++ src/com/multimedia/slidepuzzel/SettingsActivity.java
@@ -3,9 +3,7 @@ package com.multimedia.slidepuzzel;
 import com.multimedia.slidepuzzel.application.SharedApplication;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
