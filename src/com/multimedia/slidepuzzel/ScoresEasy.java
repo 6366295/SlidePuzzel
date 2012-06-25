230479738bc3048feafe30f93645582b8431d2d0
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-06-25 17:36:02 +0200

    Highscores easy

--- src/com/multimedia/slidepuzzel/ScoresEasy.java
+++ src/com/multimedia/slidepuzzel/ScoresEasy.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -19,29 +20,24 @@ public class ScoresEasy extends Activity {
         
         TableLayout layout = (TableLayout) findViewById(R.id.tableLayout1);
         
+        LayoutInflater inflater = getLayoutInflater();
+        
         HighscoreEntry[] entries = ((SharedApplication) getApplication()).dataManager.getHighscore("EASY", 5);
         for(int i = 0; i < entries.length; i++){
         	if(entries[i] == null)break;
         	Log.d("Highscore", "Add 1");
-        	TableRow row = new TableRow(this);
+        	TableRow row = (TableRow)inflater.inflate(R.layout.tablerow, layout, false);
         	
-        	TextView id = new TextView(this);
-        	TextView name = new TextView(this);
-        	TextView time = new TextView(this);
+        	TextView id = (TextView) row.findViewById(R.id.table_id);
+            id.setText("" + (i + 1));
+
+            TextView name = (TextView) row.findViewById(R.id.table_name);
+            name.setText(entries[i].getName());
+            
+            TextView time = (TextView) row.findViewById(R.id.table_time);
+            time.setText(entries[i].getTime() + "");
         	
-        	id.setText("" + (i + 1));
-        	name.setText(entries[i].getName());
-        	time.setText("" + entries[i].getTime());
-        	
-        	id.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 0.2f));
-        	name.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 0.5f));
-        	time.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 0.3f));
-        	
-        	row.addView(id);
-        	row.addView(name);
-        	row.addView(time);
-        	
-        	layout.addView(row, new TableLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+        	layout.addView(row);
         }
     }
 }
