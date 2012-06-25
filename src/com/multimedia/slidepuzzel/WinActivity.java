091fcd7078cd3f06e2464b599ed3dbc59967ceff
unknown <Ren@Ren-PC.(none)>
2012-06-25 17:41:21 +0200

    not working winactivity

--- src/com/multimedia/slidepuzzel/WinActivity.java
+++ src/com/multimedia/slidepuzzel/WinActivity.java
@@ -1,26 +1,38 @@
 package com.multimedia.slidepuzzel;
 
+import com.multimedia.slidepuzzel.application.SharedApplication;
+import com.multimedia.slidepuzzel.data.HighscoreEntry;
+import com.multimedia.slidepuzzel.data.Settings;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class WinActivity extends Activity {
+
+	private SharedApplication app;
+	private HighscoreEntry h;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-		int totaltime, sec, min;
+		int totaltime1, totaltime2, sec, min;
 		min = 0;
+        app = (SharedApplication) getApplication();
 		
         super.onCreate(savedInstanceState);
         setContentView(R.layout.win);
         
         Intent sender=getIntent();
-        totaltime =sender.getExtras().getInt("time");
-        sec = totaltime %60;
-        while(totaltime > 60){
-        	totaltime = totaltime - 60;
+        totaltime1 =sender.getExtras().getInt("time");
+        totaltime2 = totaltime1;
+        sec = totaltime1 %60;
+        while(totaltime1 > 60){
+        	totaltime1 = totaltime1 - 60;
         	min++;
         }
         
@@ -33,6 +45,7 @@ public class WinActivity extends Activity {
         Button back = (Button) findViewById(R.id.back5);
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                app.dataManager.insertHighscore(h);
                 finish();
             }
         });
@@ -40,10 +53,31 @@ public class WinActivity extends Activity {
         Button newgame = (Button) findViewById(R.id.newgame);
         newgame.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                app.dataManager.insertHighscore(h);
                 finish();
                 Intent intent = new Intent(WinActivity.this, GameActivity.class);
             	startActivity(intent);
             }
         });
+        
+        EditText et = (EditText) findViewById(R.id.entername);
+        String theText = et.getText().toString();
+        
+		Settings s = new Settings();
+		if(app.diff.equals("EASY"))
+			s.setDifficulty("EASY");
+		else if(app.diff.equals("NORMAL"))
+			s.setDifficulty("NORMAL");
+		else if(app.diff.equals("HARD"))
+			s.setDifficulty("HARD");
+		if(app.size == 3)
+			s.setSize(3);
+		else if(app.size == 4)
+			s.setSize(4);
+		h.setSettings(s);
+        h.setTime(totaltime2);
+        h.setName(theText);
+    	Log.d("Highscore", "Score Added, NAME: " + theText + " TIME: " + totaltime2);
+        
     }
 }
